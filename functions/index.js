a65fb3249041896f57832aab9db85fe68c566741
MD. Rakibul Hassan <tanvirakibul@gmail.com>
2020-10-26 20:09:14 +0600

    fixed errors

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
 const functions = require('firebase-functions');
+const config = functions.config();
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
-const config = functions.config();
-
 admin.initializeApp();
 
-const transporter = nodemailer.createTransport({ service: "Gmail",
-auth: { user: config.user.email, pass: config.user.password}})
+const transporter = nodemailer.createTransport({ service: "Gmail", auth: {
+    user: config.user.email, pass:config.user.password
+}})
 
-let mailOptions = {from: 'Arc Development', to: "tanvirakibul@gmail.com", 
-subject: 'Testting Nodemailer', text: 'Test Successful'}
+let mailOptions = {from: 'Arc Development', to: "tanvirakibul@gmail.com",
+subject: 'Testing nodemailer', text: 'Test successful'}
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -19,7 +19,7 @@ exports.sendMail = functions.https.onRequest((request, response) => {
       if (error) {
           response.send(error)
       } else {
-          response.send("Message Sent Successfully")
+          response.send("Message sent successfully")
       }
   })
 });
