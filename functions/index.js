05648cf25e26eaf2871a24285472c152cbedb569
MD. Rakibul Hassan <tanvirakibul@gmail.com>
2020-10-26 19:36:47 +0600

    added the functions

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,25 @@
 const functions = require('firebase-functions');
-const config = functions.config();
 const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const config = functions.config();
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const transporter = nodemailer.createTransport({ service: "Gmail",
+auth: { user: config.user.email, pass: config.user.password}})
+
+let mailOptions = {from: 'Arc Development', to: "tanvirakibul@gmail.com", 
+subject: 'Testting Nodemailer', text: 'Test Successful'}
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.sendMail = functions.https.onRequest((request, response) => {
+  transporter.sendMail(mailOptions, error => {
+      if (error) {
+          response.send(error)
+      } else {
+          response.send("Message Sent Successfully")
+      }
+  })
+});
