803cc73b0b1f9df79133187637213aa8a27efa61
MD. Rakibul Hassan <tanvirakibul@gmail.com>
2020-10-26 20:14:53 +0600

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const config = functions.config();
+const cors = require("cors")({origin: true})
 const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
 admin.initializeApp();
@@ -15,6 +16,9 @@ subject: 'Testing nodemailer', text: 'Test successful'}
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.sendMail = functions.https.onRequest((request, response) => {
+    cors( request, response, () => {
+
+
   transporter.sendMail(mailOptions, error => {
       if (error) {
           response.send(error)
@@ -22,4 +26,5 @@ exports.sendMail = functions.https.onRequest((request, response) => {
           response.send("Message sent successfully")
       }
   })
+})
 });
