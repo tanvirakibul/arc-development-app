07136938693adf4cb6f0276c164824b799f00e39
MD. Rakibul Hassan <tanvirakibul@gmail.com>
2020-10-26 17:55:26 +0600

    added firebase functionalities

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const config = functions.config();
 const admin = require("firebase-admin");
 
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
