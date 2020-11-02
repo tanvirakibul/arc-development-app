35f293197c71356a807aa545af101672f428d4df
MD. Rakibul Hassan <tanvirakibul@gmail.com>
2020-11-02 17:48:09 +0600

    added the dynamic state for questions

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -10,7 +10,7 @@ import check from "../assets/check.svg";
 import send from "../assets/send.svg";
 import software from "../assets/software.svg";
 import mobile from "../assets/mobile.svg";
-import website from "../assets/mobile.svg";
+import website from "../assets/website.svg";
 import backArrow from "../assets/backArrow.svg";
 import forwardArrow from "../assets/forwardArrow.svg";
 import backArrowDisabled from "../assets/backArrowDisabled.svg";
@@ -34,23 +34,61 @@ import biometrics from "../assets/biometrics.svg";
 import estimateAnimation from "../animations/estimateAnimation/data.json";
 
 const useStyles = makeStyles((theme) => ({
-    icon: {
-        width: "12em",
-        height: "10em"
+  icon: {
+    width: "12em",
+    height: "10em",
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    backgroundColor: theme.palette.common.orange,
+    height: 50,
+    width: 225,
+    fontSize: "1.25rem",
+    marginTop: "5em",
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.light,
     },
-    estimateButton: {
-        ...theme.typography.estimate,
-        borderRadius: 50,
-        backgroundColor: theme.palette.common.orange,
-        height: 50,
-        width: 225,
-        fontSize: "1.25rem",
-        marginTop: "5em",
-        "&:hover" : {
-            backgroundColor: theme.palette.secondary.light
-        }
-    }
+  },
 }));
+
+// state for the questions
+const defaultQuestions = [
+  {
+    id: 1,
+    title: "Which Service are you interested in?",
+    active: true,
+    options: [
+      {
+        id: 1,
+        title: "Custom Software Development",
+        subtitle: null,
+        icon: software,
+        iconAlt: "three floating screens",
+        selected: false,
+        cost: 0,
+      },
+      {
+        id: 2,
+        title: "iOS/Android App Development",
+        subtitle: null,
+        icon: mobile,
+        iconAlt: "phones and teblet outline",
+        selected: false,
+        cost: 0,
+      },
+      {
+        id: 3,
+        title: "Website Development",
+        subtitle: null,
+        icon: website,
+        iconAlt: "computer outline",
+        selected: false,
+        cost: 0,
+      },
+    ],
+  },
+];
 
 export default function Estimate() {
   const classes = useStyles();
@@ -69,79 +107,88 @@ export default function Estimate() {
     <Grid container direction="row">
       {/* Estimate title & Animations */}
       <Grid item container direction="column" lg>
-        <Grid item
-        style={{marginTop: "2em",
-        marginLeft: "5em"}}>
+        <Grid item style={{ marginTop: "2em", marginLeft: "5em" }}>
           <Typography variant="h2">Estimate</Typography>
         </Grid>
-        <Grid item style={{marginRight: "10em", maxWidth: "50em",
-    marginTop: "7.5em"}}>
+        <Grid
+          item
+          style={{ marginRight: "10em", maxWidth: "50em", marginTop: "7.5em" }}
+        >
           <Lottie options={defaultOptions} height="100%" width="100%" />
         </Grid>
       </Grid>
 
       {/* Questions */}
-      <Grid item container direction="column" lg
-      alignItems="center"
-      style={{marginRight:"2em", marginBottom:"10em"}}>
-        <Grid item>
-            <Typography variant="h2" align="center" 
-            style={{fontWeight: 500, fontSize:"2.25em", marginBottom: "2.5em",
-        marginTop: "5em"}}
-            gutterBottom>
-                Which service are you interested in?
-            </Typography>
-        </Grid>
-        <Grid item container>
-            <Grid item container direction="column" md>
-                <Grid item style={{maxWidth: "12em"}}>
-                    <Typography variant="h6" align="center">
-                        Custom Software Development
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <img src={software} alt="three floating screens"
-                    className={classes.icon} />
-                </Grid>
-            </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        lg
+        alignItems="center"
+        style={{ marginRight: "2em", marginBottom: "10em" }}
+      >
+        {defaultQuestions
+          .filter((question) => question.active)
+          .map((question, index) => (
+            <React.Fragment key={index}>
+              <Grid item>
+                <Typography
+                  variant="h2"
+                  align="center"
+                  style={{
+                    fontWeight: 500,
+                    fontSize: "2.25em",
+                    marginTop: "5em",
+                  }}
+                  gutterBottom
+                >
+                  {question.title}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  align="center"
+                  style={{ marginBottom: "2.5em" }}
+                  gutterBottom
+                >
+                  {question.subtitle}
+                </Typography>
+              </Grid>
+              <Grid item container>
+                {question.options.map((options) => (
+                  <Grid item container direction="column" md>
+                    <Grid item style={{ maxWidth: "12em" }}>
+                      <Typography variant="h6" align="center">
+                        {options.title}
+                      </Typography>
+                      <Typography variant="caption" align="center">
+                        {options.subtitle}
+                      </Typography>
+                    </Grid>
+                    <Grid item>
+                      <img
+                        src={options.icon}
+                        alt={options.iconAlt}
+                        className={classes.icon}
+                      />
+                    </Grid>
+                  </Grid>
+                ))}
+              </Grid>
+            </React.Fragment>
+          ))}
 
-            <Grid item container direction="column" md>
-                <Grid item style={{maxWidth: "12em"}}>
-                    <Typography variant="h6" align="center">
-                        iOS/Android App Development
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <img src={mobile} alt="phones and tables outlines"
-                    className={classes.icon} />
-                </Grid>
-            </Grid>
-
-            <Grid item container direction="column" md>
-                <Grid item style={{maxWidth: "12em"}}>
-                    <Typography variant="h6" align="center" 
-                    style={{marginBottom: "1em"}}>
-                        Website Development
-                    </Typography>
-                </Grid>
-                <Grid item>
-                    <img src={website} alt="computer screen"
-                    className={classes.icon} />
-                </Grid>
-            </Grid>
-        </Grid>
-        <Grid item container justify="space-between" style={{width:"15em"}}>
-            <Grid item>
-                <img src={backArrow} alt="Previous question" />
-            </Grid>
-            <Grid item>
-                <img src={forwardArrow} alt="Next question" />
-            </Grid>
+        <Grid item container justify="space-between" style={{ width: "15em" }}>
+          <Grid item>
+            <img src={backArrow} alt="Previous question" />
+          </Grid>
+          <Grid item>
+            <img src={forwardArrow} alt="Next question" />
+          </Grid>
         </Grid>
         <Grid item>
-            <Button variant="contained" className={classes.estimateButton}>
-                Get Estimate
-            </Button>
+          <Button variant="contained" className={classes.estimateButton}>
+            Get Estimate
+          </Button>
         </Grid>
       </Grid>
 
